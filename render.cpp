1744fb050c5dba4d4230c48a75f2c709884db402
adanlbenito <adanl.benito@gmail.com>
2018-11-28 23:38:36 +0000

    Added example

--- render.cpp
+++ render.cpp
@@ -1,16 +1,36 @@
 #include <Bela.h>
+#include <KarplusStrong.h>
+#include <Scope/Scope.h>
+
+KarplusStrong string;
+Scope scope;
 
 bool setup(BelaContext *context, void *userData)
 {
+	if(string.setup(2, context->audioSampleRate) != 0) {
+		fprintf(stderr, "Unable to initialise touch sensor\n");
+		return false;
+	}
+	
+	scope.setup(2, context->audioSampleRate);
+	
 	return true;
 }
 
 void render(BelaContext *context, void *userData)
 {
+	float ksOut;
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+		
+		ksOut = string.process(audioRead(context, n, 0));
 
+		for(unsigned int ch = 0; ch < context->audioOutChannels; ch++){
+			audioWrite(context, n, ch, ksOut);
+		}
+	}
 }
 
 void cleanup(BelaContext *context, void *userData)
 {
-
+	string.cleanup();
 }

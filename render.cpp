8f571362a37ed29356745d01b0472cb2aca3c619
Giulio Moro <giuliomoro@yahoo.it>
2018-11-30 20:30:48 +0000

    Added other input channel for mic, taken the analog loop outside the channels loop, changed frequency ranges

--- render.cpp
+++ render.cpp
@@ -4,21 +4,23 @@
 #include <math.h>
 #include <math_neon.h>
 
-KarplusStrong string;
-Scope scope;
+KarplusStrong gPiezoString;
+KarplusStrong gMicString;
+Scope gScope;
 
 float gOutputGain = 0.55;
-int gAudioFramesPerAnalogFrame;
+float gFreqRatio = 1.333;
 
-float gMinFrequency = 100;
-float gFreqRange[2] = { 261.63, 523.25 };
-float gDampingRange[2] = { 0.86, 0.998 };
-
-int gFsrInput = 0;
-int gPotInput = 1;
-
+float gFreqRange[2] = { 130.8165, 523.25 };
+float gDampingRange[2] = { 0.86, 0.992 };
+unsigned int gPiezoChannel = 0;
+unsigned int gMicChannel = 1;
+int gFsrChannel = 0;
+int gPotChannel = 1;
 float gAnalogFullScale = 3.3/4.096;
 float gFsrRange[2] = { 0.4, gAnalogFullScale };
+
+int gAudioFramesPerAnalogFrame;
 
 float logMap(float input, float inRange0, float inRange1, float outRange0, float outRange1)
 {
@@ -31,12 +33,10 @@ float logMap(float input, float inRange0, float inRange1, float outRange0, float
 
 bool setup(BelaContext *context, void *userData)
 {
-	if(string.setup(context->audioSampleRate, gMinFrequency, 432) != 0) {
-		fprintf(stderr, "Unable to initialise touch sensor\n");
-		return false;
-	}
+	gPiezoString.setup(context->audioSampleRate, gFreqRange[0], 432);
+	gMicString.setup(context->audioSampleRate, gFreqRange[0], 432.f * gFreqRatio);
 
-	scope.setup(6, context->audioSampleRate);
+	gScope.setup(4, context->audioSampleRate);
 	
 	gAudioFramesPerAnalogFrame = context->audioFrames / context->analogFrames;
 
@@ -45,39 +45,40 @@ bool setup(BelaContext *context, void *userData)
 
 void render(BelaContext *context, void *userData)
 {
-	float ksOut;
 	float damping, frequency;
 	float fsrVal, potVal;
 	
 	for(unsigned int n = 0; n < context->audioFrames; n++) {
 		
-		float piezoInput = audioRead(context, n, 0);
-		ksOut = string.process(piezoInput);
-		
-		for(unsigned int ch = 0; ch < context->audioOutChannels; ch++){
-			audioWrite(context, n, ch, gOutputGain * ksOut);
-			
-			if(gAudioFramesPerAnalogFrame && !(n % gAudioFramesPerAnalogFrame)) {
-				// read analog inputs and update frequency and damping
-				// Depending on the sampling rate of the analog inputs, this will
-				//	happen every audio frame (if it is 44100)
-				//	or every two audio frames (if it is 22050)
-				
-				fsrVal = analogRead(context, n/gAudioFramesPerAnalogFrame, gFsrInput);
-				fsrVal = constrain(fsrVal, gFsrRange[0], gFsrRange[1]);
-				damping = logMap(fsrVal, gFsrRange[1], gFsrRange[0], gDampingRange[0], gDampingRange[1]);
-				potVal = analogRead(context, n/gAudioFramesPerAnalogFrame, gPotInput);
-				frequency = map(potVal, 0, 1, gFreqRange[0], gFreqRange[1]);
-				
-				string.setFrequency(frequency);
-				string.setDamping(damping);
-			}
-			scope.log(piezoInput, ksOut, fsrVal, potVal);
+		if(gAudioFramesPerAnalogFrame && !(n % gAudioFramesPerAnalogFrame)) {
+			// read analog inputs and update frequency and damping
+			// Depending on the sampling rate of the analog inputs, this will
+			//	happen every audio frame (if it is 44100)
+			//	or every two audio frames (if it is 22050)
+
+			fsrVal = analogRead(context, n/gAudioFramesPerAnalogFrame, gFsrChannel);
+			fsrVal = constrain(fsrVal, gFsrRange[0], gFsrRange[1]);
+			damping = logMap(fsrVal, gFsrRange[1], gFsrRange[0], gDampingRange[0], gDampingRange[1]);
+			potVal = analogRead(context, n/gAudioFramesPerAnalogFrame, gPotChannel);
+			frequency = map(potVal, 0, 1, gFreqRange[0], gFreqRange[1]);
+
+			gPiezoString.setFrequency(frequency);
+			gPiezoString.setDamping(damping);
+			gMicString.setFrequency(frequency * gFreqRatio);
+			gMicString.setDamping(damping);
 		}
+
+		float piezoInput = audioRead(context, n, gPiezoChannel);
+		float micInput = audioRead(context, n, gMicChannel);
+		float piezoStringOut = gPiezoString.process(piezoInput);
+		float micStringOut = gMicString.process(micInput);
+
+		for(unsigned int ch = 0; ch < context->audioOutChannels; ch++){
+			audioWrite(context, n, ch, gOutputGain * (piezoStringOut + micStringOut));
+		}
+		gScope.log(micInput, micStringOut, fsrVal, potVal);
 	}
 }
 
 void cleanup(BelaContext *context, void *userData)
-{
-	string.cleanup();
-}
+{}
